b5520e978ba8a2f1573977156a8dc9a3d3d5e2e8
Mehmet Keçeci <71701959+mehmet-k@users.noreply.github.com>
2022-05-26 22:51:39 +0300

    generate eq fixed

--- src/Core/Generate.java
+++ src/Core/Generate.java
@@ -8,57 +8,34 @@ public class Generate {
          return (int) Math.floor(Math.random()*(max-min+1)+min);
     }
 
-
-    public static int Calculate2(int op,int value1, int value2){
-        int result;
-        if(op == 0){
-            result = value1 + value2;;
-        }
-        else if( op == 1){
-            result = value1-value2;
-        }
-        else if(op==2){
-            result = value1*value2;
-        }
-        else {
-            if(value2 == 0 ||(value1 %value2) != 0 ){
-                return -9999;
-            }
-            else{
-                result=value1/value2;
-            }
-
-        }
-        return result;
-    }
-    public static int Calculate(int value1, int value2,ArrayList<Character>ops){
-        int result;
+    public static void selectOP(ArrayList<Character>ops){
         int op = generateRandom(0,6);
-        if(op == 0){
+        if(op == 0) ops.add('+');
+        else if( op == 1) ops.add('-');
+        else if(op==2) ops.add('*');
+        else ops.add('/');
+    }
+
+    public static int calculateEquationResult(int value1, int value2,char op){
+        int result;
+        if(op == '+'){
             result = value1 + value2;
-            ops.add('+');
         }
-        else if( op == 1){
+        else if( op == '-'){
             result = value1-value2;
-            ops.add('-');
         }
-        else if(op==2){
+        else if(op=='*'){
             result = value1*value2;
-            ops.add('*');
         }
         else {
             if(value2 == 0 ||(value1 %value2) != 0 ){
-                ops.add('?');
                 return -9999;
             }
             else{
                 result=value1/value2;
-                ops.add('/');
             }
 
         }
-
-
         return result;
     }
 
@@ -76,21 +53,23 @@ public class Generate {
             eq.add(generateRandom(min,max));
             eq.add(generateRandom(min,max));
             eq.add(generateRandom(min,max));
-            eq.add(Calculate(eq.get(0),eq.get(1),ops));
+            selectOP(ops);
+            eq.add(calculateEquationResult(eq.get(0),eq.get(1),ops.get(0)));
             System.out.println(eq);
             if(eq.get(3) != -9999){
-                equationString = Integer.toString(eq.get(0)) + ops.get(0) + Integer.toString(eq.get(1)) + "=" + Integer.toString(eq.get(3));
+                equationString = Integer.toString(eq.get(0)) + ops.get(0) + (eq.get(1)) + "=" + (eq.get(3));
                 if(equationString.length()<7){
-                    eq.set(3,Calculate(eq.get(3),eq.get(2),ops));
+                    selectOP(ops);
+                    eq.set(3,calculateEquationResult(eq.get(3),eq.get(2),ops.get(1)));
                     System.out.println(eq);
                     if(eq.get(3) != -9999){
-                        if((ops.get(1).equals('/')  || ops.get(1).equals('*') ) && !(ops.get(0).equals('/')  || !ops.get(0).equals('*'))){
-                            eq.set(3,Calculate2(eq.get(1),eq.get(2),ops.get(1)));
-                            eq.set(3,Calculate2(eq.get(3),eq.get(0),ops.get(0)));
+                        if((ops.get(1).equals('/')  || ops.get(1).equals('*') ) && (ops.get(0).equals('+')  || ops.get(0).equals('-'))){
+                            eq.set(3,calculateEquationResult(eq.get(1),eq.get(2),ops.get(1)));
+                            if(eq.get(3) != -9999)
+                                eq.set(3,calculateEquationResult(eq.get(3),eq.get(0),ops.get(0)));
                         }
                         System.out.println(eq);
-                        equationString = Integer.toString(eq.get(0)) + ops.get(0) + Integer.toString(eq.get(1)) + ops.get(1) + Integer.toString(eq.get(2))+
-                                "=" + Integer.toString(eq.get(3));
+                        equationString = Integer.toString(eq.get(0)) + ops.get(0) + (eq.get(1)) + ops.get(1) + (eq.get(2))+ "=" + (eq.get(3));
                     }
                 }
             }
