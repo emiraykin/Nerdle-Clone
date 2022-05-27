07725c8595f8bb09a2fea1483f3329f603240c27
emir <cagri.aykin@std.yildiz.edu.tr>
2022-05-27 04:09:37 +0300

    newGame work .Backend bugs,buttons will be fixed

--- src/Frontend/NewGame.java
+++ src/Frontend/NewGame.java
@@ -12,36 +12,41 @@ import java.util.ArrayList;
 import static Core.Equation.EquationControls.checkCharactersStatus;
 import static Core.Equation.EquationControls.isEquationResultIsTrue;
 import static Core.Generate.GenerateEquation;
+import static Core.Statistics.readStatistics;
 import static Core.Statistics.writeStatistics;
 
 public class NewGame {
     int currentLine = 0;
     JButton submit = new JButton("Guess");
+    JButton back = new JButton("Back");
+
     JLabel CorrOrWrong = new JLabel("---");
     String generatedEquation ;
     private static JTextField[][] index = new JTextField[9][9];
     JFrame frame;
     public NewGame (){
         generatedEquation = GenerateEquation();
-        Statistics stats = new Statistics();
+       /* Statistics stats = new Statistics();
         stats.setAvgFinishAtLines(2);
         stats.setLosses(3);
         stats.setVictory(8);
         stats.setAvgSuccessTime(null);
         stats.setUnfinishedGames(1);
-        writeStatistics(stats);
+        writeStatistics(stats);*/
 
         frame = new JFrame("Nerdle");// tum pencere ve ismi
         //frame.getContentPane().add(draw);
         frame.setSize(750,500);
         //submit.addActionListener(this);
         JPanel board = new JPanel();
+
         //JPanel panel = new JPanel();
         board.setLayout(new GridLayout (6,generatedEquation.length()));
         for (int i =0;i<6;i++)
             for (int j=0;j<generatedEquation.length();j++){
                 index[i][j]=new JTextField(1);
                 index[i][j].setText("");
+                index[i][j].setBounds(10,0,0,0);
                 //index[i][j].setEditable(true);
                 if(i!=currentLine){
                     index[i][j].setEditable(false);
@@ -49,16 +54,22 @@ public class NewGame {
 
                 board.add(index[i][j]);
             }
-        index[3][3].requestFocusInWindow();
 
         frame.getContentPane().add(board);
         frame.getContentPane().add(submit,"South");
+        frame.getContentPane().add(back,"West");
+
         frame.getContentPane().add(CorrOrWrong, "North");
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         submit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 submitTestActionPerformed(evt);
+            }
+        });
+        back.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backActionPerformed(evt);
             }
         });
         CorrOrWrong.setText(generatedEquation);
@@ -69,13 +80,18 @@ public class NewGame {
 
     private void submitTestActionPerformed(java.awt.event.ActionEvent evt) {
         String input = "";
-        for(int j=0;j<generatedEquation.length();j++){
+        int j = 0;
+        while (j < generatedEquation.length() && index[currentLine][j].getText().compareTo("")!=0){
             input =input+index[currentLine][j].getText().charAt(0);
+            j++;
         }
+       /* for(int j=0;j<generatedEquation.length();j++){
+            input =input+index[currentLine][j].getText().charAt(0);
+        }*/
         System.out.println("---------------------");
         System.out.println(input);
 
-        if(!isEquationResultIsTrue(input)){
+        if(input.length() != generatedEquation.length() || !isEquationResultIsTrue(input) ){
             JOptionPane.showMessageDialog(frame, "This equation is not correct",
                     "WARNING", JOptionPane.ERROR_MESSAGE);
         }
@@ -86,11 +102,14 @@ public class NewGame {
             for (int i = 0; i < generatedEquation.length() ; i++) {
                 switch (status.get(i)){
                     case 0:
-                        for (int j = currentLine; j < 6 ; j++) {
+                        /*for (int j = currentLine; j < 6 ; j++) {
                             index[j][i].setText(String.valueOf(input.charAt(i)));
                             index[j][i].setEditable(false);
                             index[j][i].setBackground(Color.green);
-                        }
+                        }*/
+                        index[currentLine][i].setBackground(Color.green);
+                        index[currentLine][i].setEditable(false);
+
                         break;
                     case 1:
                         index[currentLine][i].setEditable(false);
@@ -104,9 +123,45 @@ public class NewGame {
 
                 }
             }
+            if(input.compareTo(generatedEquation)==0){
+                JOptionPane.showMessageDialog(frame, " You Won.\n ",
+                        "Congratulations!", JOptionPane.INFORMATION_MESSAGE);
+                Statistics stats = new Statistics();
+                stats=readStatistics();
+                stats.setAvgSuccessTime(null); /** burayı UNUTMA */
+                stats.setAvgFinishAtLines( ((stats.getAvgFinishAtLines() * stats.getVictory())  +currentLine + 1) / (stats.getVictory()+1));
+                stats.setVictory(stats.getVictory()+1);
+
+                writeStatistics(stats);
+
+            }
+            else {
+                if(currentLine==5){
+                    JOptionPane.showMessageDialog(frame, "You lost.\n ",
+                            "Game Over", JOptionPane.INFORMATION_MESSAGE);
+                    Statistics stats = new Statistics();
+                    stats=readStatistics();
+                    stats.setLosses(stats.getLosses()+1);
+                    writeStatistics(stats);
+                }
+                else{
+                    currentLine++;
+                    for (int i = 0; i < generatedEquation.length(); i++) {
+                        index[currentLine][i].setEditable(true);
+
+                    }
+                }
+
+            }
+
         }
     }
 
+    private void backActionPerformed(java.awt.event.ActionEvent evt) {
+        GUI gui = new GUI();
+        gui.setVisible(true);
+        frame.dispose();
+    }
 
     public static int[][] submit(){
         int[][]result = new int[9][9];
