78f284e607db04c7884cf5a0162870bc1d2e93d9
Mehmet Keçeci <71701959+mehmet-k@users.noreply.github.com>
2022-05-29 18:14:02 +0300

    regex control added

--- src/Core/Equation/EquationControls.java
+++ src/Core/Equation/EquationControls.java
@@ -1,6 +1,7 @@
 package Core.Equation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class EquationControls {
 
@@ -49,14 +50,16 @@ public class EquationControls {
         int i;
         ArrayList<Character> ops = new ArrayList<>(2);
         ArrayList<Integer> numbers = new ArrayList<>();
-        for (i=0;i<Equation.length();i++)
-            if(EquationUtilities.isOperator(Equation.charAt(i)))
-                ops.add(Equation.charAt(i));
-
-        String[] tokens = EquationUtilities.splitEquationStringToNumbers(Equation);
-        for(i=0;i<tokens.length;i++){
-            numbers.add(Integer.parseInt(tokens[i]));
+        for (i=0;i<Equation.length();i++){
+            if(Equation.charAt(i) == '=') break;
+            if(EquationUtilities.isOperator(Equation.charAt(i))) ops.add(Equation.charAt(i));
         }
+        String[] tokens = EquationUtilities.splitEquationStringToNumbers(Equation);
+        for (String token : tokens) {
+            if(!token.equals(""))
+                numbers.add(Integer.parseInt(token));
+        }
+        if(Equation.charAt(i+1) == '-') numbers.set(numbers.size()-1,Integer.parseInt(String.valueOf(- numbers.get(numbers.size() - 1))));
         if(ops.size() == 1){
             return numbers.get(2) == EquationUtilities.calculateEquationResult(numbers.get(0), numbers.get(1), ops.get(0));
         }
@@ -72,4 +75,25 @@ public class EquationControls {
             return i == numbers.get(3);
         }
     }
+
+    public static boolean EquationRegexControl(String Equation){
+        int i,equalsCounter=0,opCounter=0;
+        //if equation too big return false
+        if(Equation.length() > 9) return false;
+        //if first or last character is an operator return false
+        if(EquationUtilities.isOperator(Equation.charAt(0)) || EquationUtilities.isOperator(Equation.charAt(Equation.length()-1)))
+            return false;
+        for(i=1;i<Equation.length();i++){
+            //if two operators are in series or an operator is right behind equals sign, return false
+            if(EquationUtilities.isOperator(Equation.charAt(i-1))  && (EquationUtilities.isOperator(Equation.charAt(i)) || Equation.charAt(i) == '='))
+                return false;
+            if (Equation.charAt(i) == '=') equalsCounter++;
+            if(EquationUtilities.isOperator(Equation.charAt(i))) opCounter++;
+        }
+        if(equalsCounter == 0 ||opCounter == 0 ||opCounter>2) return false;
+        else return true;
+    }
 }
+
+
+
