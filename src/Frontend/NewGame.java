a3ecd360af7be4c9f5f57a5ed23cdada885edcf7
emir <cagri.aykin@std.yildiz.edu.tr>
2022-05-29 03:27:19 +0300

    button interface

--- src/Frontend/NewGame.java
+++ src/Frontend/NewGame.java
@@ -15,15 +15,30 @@ import static Core.Generate.GenerateEquation;
 import static Core.Statistics.readStatistics;
 import static Core.Statistics.writeStatistics;
 
-public class NewGame {
+public class NewGame implements ActionListener{
     int currentLine = 0;
     JButton submit = new JButton("Guess");
     JButton back = new JButton("Back");
-    JPanel main = new JPanel( new GridLayout(2, 1 ) );
+    JButton btn0 = new JButton("    0   ");
+    JButton btn1 = new JButton("    1   ");
+    JButton btn2 = new JButton("    2   ");
+    JButton btn3 = new JButton( "   3   ");
+    JButton btn4 = new JButton("    4   ");
+    JButton btn5 = new JButton("    5   ");
+    JButton btn6 = new JButton("    6   ");
+    JButton btn7 = new JButton("    7   ");
+    JButton btn8 = new JButton("    8   ");
+    JButton btn9 = new JButton("    9   ");
+    JButton btnSave = new JButton("Save game");
+
+
+    JPanel main = new JPanel( new BorderLayout() );
     JLabel CorrOrWrong = new JLabel("---");
     String generatedEquation ;
     private static JTextField[][] index = new JTextField[9][9];
     JFrame frame;
+
+
     public NewGame (){
         generatedEquation = GenerateEquation();
        /* Statistics stats = new Statistics();
@@ -39,8 +54,8 @@ public class NewGame {
         frame.setSize(750,500);
         //submit.addActionListener(this);
         JPanel board = new JPanel();
-        board.setPreferredSize( new Dimension(300, 300) );
-        main.add(board);
+        board.setPreferredSize( new Dimension(300, 500) );
+        main.add(board,BorderLayout.CENTER);
         //JPanel panel = new JPanel();
         board.setLayout(new GridLayout (6,generatedEquation.length()));
         for (int i =0;i<6;i++)
@@ -57,11 +72,28 @@ public class NewGame {
             }
 
         JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setPreferredSize(new Dimension(100,100));
-        buttonsPanel.add(back);
-        buttonsPanel.add(submit);
+        buttonsPanel.setPreferredSize(new Dimension(200,100));
 
-        main.add(buttonsPanel);
+        buttonsPanel.add(btn0);
+        buttonsPanel.add(btn1);
+        buttonsPanel.add(btn2);
+        buttonsPanel.add(btn3);
+        buttonsPanel.add(btn4);
+        buttonsPanel.add(btn5);
+        buttonsPanel.add(btn6);
+        buttonsPanel.add(btn7);
+        buttonsPanel.add(btn8);
+        buttonsPanel.add(btn9);
+        buttonsPanel.add(back);
+        buttonsPanel.add(btnSave);
+        buttonsPanel.add(submit);
+        back.setBackground(Color.PINK);
+
+
+
+
+
+        main.add(buttonsPanel,BorderLayout.PAGE_END);
 
 
         frame.getContentPane().add(main);
@@ -84,6 +116,98 @@ public class NewGame {
         CorrOrWrong.setText(generatedEquation);
         frame.setVisible(true);
     }
+
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource() == submit)
+        {
+            String input = "";
+            int j = 0;
+            while (j < generatedEquation.length() && index[currentLine][j].getText().compareTo("")!=0){
+                input =input+index[currentLine][j].getText().charAt(0);
+                j++;
+            }
+       /* for(int j=0;j<generatedEquation.length();j++){
+            input =input+index[currentLine][j].getText().charAt(0);
+        }*/
+            System.out.println("---------------------");
+            System.out.println(input);
+
+            if(input.length() != generatedEquation.length() || !isEquationResultIsTrue(input) ){
+                JOptionPane.showMessageDialog(frame, "This equation is not correct",
+                        "WARNING", JOptionPane.ERROR_MESSAGE);
+            }
+            else{
+                ArrayList<Integer> status = new ArrayList<Integer>();
+                ArrayList<Boolean> ifVisited = new ArrayList<Boolean>() ;
+                checkCharactersStatus(generatedEquation,input,status,ifVisited);
+                for (int i = 0; i < generatedEquation.length() ; i++) {
+                    switch (status.get(i)){
+                        case 0:
+                        /*for (int j = currentLine; j < 6 ; j++) {
+                            index[j][i].setText(String.valueOf(input.charAt(i)));
+                            index[j][i].setEditable(false);
+                            index[j][i].setBackground(Color.green);
+                        }*/
+                            index[currentLine][i].setBackground(Color.green);
+                            index[currentLine][i].setEditable(false);
+
+                            break;
+                        case 1:
+                            index[currentLine][i].setEditable(false);
+                            index[currentLine][i].setBackground(Color.YELLOW);
+                            break;
+                        case 2:
+                            index[currentLine][i].setEditable(false);
+                            index[currentLine][i].setBackground(Color.RED);
+                            break;
+
+
+                    }
+                }
+                if(input.compareTo(generatedEquation)==0){
+                    JOptionPane.showMessageDialog(frame, " You Won.\n ",
+                            "Congratulations!", JOptionPane.INFORMATION_MESSAGE);
+                    Statistics stats = new Statistics();
+                    stats=readStatistics();
+                    stats.setAvgSuccessTime(null); /** burayı UNUTMA */
+                    stats.setAvgFinishAtLines( ((stats.getAvgFinishAtLines() * stats.getVictory())  +currentLine + 1) / (stats.getVictory()+1));
+                    stats.setVictory(stats.getVictory()+1);
+
+                    writeStatistics(stats);
+
+                }
+                else {
+                    if(currentLine==5){
+                        JOptionPane.showMessageDialog(frame, "You lost.\n ",
+                                "Game Over", JOptionPane.INFORMATION_MESSAGE);
+                        Statistics stats = new Statistics();
+                        stats=readStatistics();
+                        stats.setLosses(stats.getLosses()+1);
+                        writeStatistics(stats);
+                    }
+                    else{
+                        currentLine++;
+                        for (int i = 0; i < generatedEquation.length(); i++) {
+                            index[currentLine][i].setEditable(true);
+
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+        else if( e.getSource() == back){
+            GUI gui = new GUI();
+            gui.setVisible(true);
+            frame.dispose();
+        }
+    }
+
+
+
+
 
 
 
